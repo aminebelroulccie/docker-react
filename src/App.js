0023984c2095c132422c65d84363575a9cbc9d7d
unknown <aminebelroulccie@gmail.com>
2020-08-14 00:46:18 +0100

    added travis file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          no way ,there
+          its changed in feature branch
         </p>
         <a
           className="App-link"
